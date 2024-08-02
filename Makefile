d3604ab16c5d02f267e7332990aa3bf2dd130724
Nima Nourinejad <nima.nourinejad@gmail.com>
2024-08-02 08:41:15 +0300

    Added main.cpp and Makefile

---
+++ Makefile
@@ -0,0 +1,28 @@
+NAME = maker
+CXX = c++
+RM = rm -f
+CXXFLAGS = -Wall -Wextra -Werror
+SRCS =  main.cpp \
+
+OBJS = $(SRCS:.cpp=.o)
+
+.PHONY: all clean fclean re c98
+
+all: $(NAME)
+
+$(NAME): $(OBJS)
+	@$(CXX) $(CXXFLAGS) $(OBJS) -o $@
+
+%.o: %.cpp
+	@$(CXX) $(CXXFLAGS) -c $< -o $@
+
+clean:
+	@$(RM) $(OBJS)
+
+fclean: clean
+	@$(RM) $(NAME)
+
+re: fclean all
+
+c98: CXXFLAGS += -std=c++98
+c98: re
---
+++ main.cpp
@@ -0,0 +1,74 @@
+#include <fstream>
+#include <iostream>
+#include <ostream>
+#include <sstream>
+#include <string>
+
+int main(int argc, char **argv) {
+  if (argc != 3) {
+    std::cerr << "Usage: " << argv[0] << " <class name> <address>" << std::endl;
+    return 1;
+  } else {
+    std::stringstream raw;
+    std::string className = argv[1];
+    for (int i = 0; i < static_cast<int>(className.length()); i++) {
+      char c = static_cast<unsigned char>(className[i]);
+      if (i == 0)
+        c = std::toupper(c);
+      else
+        c = std::tolower(c);
+      raw << c;
+    }
+    className = raw.str();
+    std::string address = argv[2];
+    std::stringstream headerName;
+    headerName << address << "/" << className << ".hpp";
+    std::stringstream sourceName;
+    sourceName << address << "/" << className << ".cpp";
+    std::ofstream header(headerName.str().c_str());
+    std::ofstream source(sourceName.str().c_str());
+    std::stringstream upper;
+    for (int i = 0; i < static_cast<int>(className.length()); i++) {
+      char c = static_cast<unsigned char>(className[i]);
+      c = std::toupper(c);
+      upper << c;
+    }
+    std::string upperClassName = upper.str();
+    header << "#ifndef " << upperClassName << "_HPP" << std::endl;
+    header << "#define " << upperClassName << "_HPP" << std::endl;
+    header << std::endl;
+    header << "#include <iostream>" << std::endl;
+    header << "#include <string>" << std::endl;
+    header << "#include <sstream>" << std::endl;
+    header << "#include <ostream>" << std::endl;
+    header << "#include <fstream>" << std::endl;
+    header << std::endl;
+    header << "class " << className << " {" << std::endl;
+    header << "public:" << std::endl;
+    header << "  " << className << "();" << std::endl;
+    header << "  ~" << className << "();" << std::endl;
+    header << "  " << className << "(const " << className << " &src);"
+           << std::endl;
+    header << "  " << className << " &operator=(const " << className
+           << " &src);" << std::endl;
+    header << std::endl;
+    header << "private:" << std::endl;
+    header << "};" << std::endl;
+    header << std::endl;
+	header << "std::ostream &operator<<(std::ostream &out, const " << className << " &src);" << std::endl;
+    header << std::endl;
+	header << "#endif" << std::endl;
+    header.close();
+    source << "#include \"" << className << ".hpp\"" << std::endl;
+    source << std::endl;
+    source << className << "::" << className << "() {}" << std::endl;
+    source << className << "::~" << className << "() {}" << std::endl;
+    source << className << "::" << className << "(const " << className
+           << " &src) {}" << std::endl;
+    source << className << " &" << className << "::operator=(const "
+           << className << " &src) {}" << std::endl;
+	source << "std::ostream &operator<<(std::ostream &out, const " << className << " &src) {}" << std::endl;
+    source.close();
+    return 0;
+  }
+}
